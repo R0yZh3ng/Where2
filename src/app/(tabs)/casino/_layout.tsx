11f01adc931a83001d4f0283b504c2fd08e6672a
R0yZh3ng <royyi2006@gmail.com>
2026-07-30 16:46:25 -0700

    game cards pressable created

--- src/app/(tabs)/casino/_layout.tsx
+++ src/app/(tabs)/casino/_layout.tsx
@@ -2,8 +2,11 @@ import { Stack } from "expo-router"
 
 export default function CasinoLayout() {
   return (
-    <Stack>
-      <Stack.Screen name = "CoinFlip" options= {{ headerShown: false }}/>
+    <Stack screenOptions= {{ headerShown: false }}>
+      <Stack.Screen name = "coin" options= {{ headerShown: false }}/>
+      <Stack.Screen name = "wheel" options = {{ headerShown: false }}/>
+      <Stack.Screen name = "finger" options = {{ headerShown: false }}/>
+      <Stack.Screen name = "number" options = {{ headerShown: false }}/>
     </Stack>
   );
 }
--- src/app/(tabs)/casino/index.tsx
+++ src/app/(tabs)/casino/index.tsx
@@ -1,10 +1,63 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, StatusBar, ImageBackground, Pressable } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function Casino() {
+export default function Casino({ title, onPress, backgroundImage }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>The Casino</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top']}>
+        <ScrollView style={styles.scrollView}>
+          <Pressable 
+            onPress = {onPress}
+            style = {({ pressed }) => [
+              styles.card,
+              pressed && styles.cardPressed,
+            ]}
+          >
+            <ImageBackground source = {backgroundImage} style= {styles.image} imageStyle = {{ borderRadius: 16 }}>
+              <Text style={styles.title}>title</Text>
+            </ImageBackground>
+          </Pressable>
+
+          <Pressable 
+            onPress = {onPress}
+            style = {({ pressed }) => [
+              styles.card,
+              pressed && styles.cardPressed,
+            ]}
+          >
+            <ImageBackground source = {backgroundImage} style= {styles.image} imageStyle = {{ borderRadius: 16 }}>
+              <Text style={styles.title}>title</Text>
+            </ImageBackground>
+          </Pressable>
+          
+          <Pressable 
+            onPress = {onPress}
+            style = {({ pressed }) => [
+              styles.card,
+              pressed && styles.cardPressed,
+            ]}
+          >
+            <ImageBackground source = {backgroundImage} style= {styles.image} imageStyle = {{ borderRadius: 16 }}>
+              <Text style={styles.title}>title</Text>
+            </ImageBackground>
+          </Pressable>
+          
+          <Pressable 
+            onPress = {onPress}
+            style = {({ pressed }) => [
+              styles.card,
+              pressed && styles.cardPressed,
+            ]}
+          >
+            <ImageBackground source = {backgroundImage} style= {styles.image} imageStyle = {{ borderRadius: 16 }}>
+              <Text style={styles.title}>title</Text>
+            </ImageBackground>
+          </Pressable>
+
+
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,5 +70,30 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#fff",
+    fontSize: 800,
+  },
+  scrollView: {
+    backgroundColor: 'blue'
+  },
+  card: {
+    height: 140,
+    width: 400,
+    borderRadius: 16,
+    overflow: 'hidden',
+    marginBottom: 12,
+  },
+  cardPressed: {
+    opacity: 0.7,
+    transform: [{ scale: 0.98 }],
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 12,
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
